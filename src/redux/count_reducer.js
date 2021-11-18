f26fb6cfb7f2701c1c90eaf26ff0f12d9df3051a
vnhomeng <vnhomeng@163.com>
2021-11-18 22:34:30 +0800

    first class

---
+++ src/redux/count_reducer.js
@@ -0,0 +1,17 @@
+/**
+ * 1.该文件是用于创建一个为Count租价您服务的reducer,reducer的本质就是一个函数
+ * 2.reducer函数会接受两个参数,分别为:之前的状态(preState),动作对象(action)
+ */
+const initState = 0
+
+export default function countReducer(preState = initState, action) {
+	const { type, data } = action
+	switch (type) {
+		case 'increment': // 如果为加
+			return preState + data
+		case 'decrement':
+			return preState - data // 如果为减
+		default:
+			return preState
+	}
+}
